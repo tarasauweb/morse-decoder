e69ea1f1f61c3af89aff97a251baa71eac5071a1
Artur Tarasau <tarasov.a.web@gmail.com>
2023-01-18 09:54:42 +0300

    compolite

--- src/index.js
+++ src/index.js
@@ -1,60 +1,64 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+    '.-': 'a',
+    '-...': 'b',
+    '-.-.': 'c',
+    '-..': 'd',
+    '.': 'e',
+    '..-.': 'f',
+    '--.': 'g',
+    '....': 'h',
+    '..': 'i',
+    '.---': 'j',
+    '-.-': 'k',
+    '.-..': 'l',
+    '--': 'm',
+    '-.': 'n',
+    '---': 'o',
+    '.--.': 'p',
+    '--.-': 'q',
+    '.-.': 'r',
+    '...': 's',
+    '-': 't',
+    '..-': 'u',
+    '...-': 'v',
+    '.--': 'w',
+    '-..-': 'x',
+    '-.--': 'y',
+    '--..': 'z',
+    '.----': '1',
+    '..---': '2',
+    '...--': '3',
+    '....-': '4',
+    '.....': '5',
+    '-....': '6',
+    '--...': '7',
+    '---..': '8',
+    '----.': '9',
+    '-----': '0',
 };
 
 function decode(expr) {
     let arr = []
-    let result = null
-    for(let i = 0; i<=expr.length ; i = i + 10){
-        arr.push(expr.slice(i ,  i + 10))
+    let reg1 = /00/g
+    let reg2 = /10/g
+    let reg3 = /11/g
+    let res = []
+    let words = []
+    for (let i = 0; i < expr.length; i = i + 10) {
+        arr.push(expr.slice(i, i + 10))
     }
-    result = arr.map(item=>{
-        return item.replace(/00/g, '').replace(/11/g, '-').replace(/10/g, '.')
-    }).map(i=>{
-        if(i === '**********'){
-            return i = ' '
-        }
-        else{
-            return i = MORSE_TABLE[i]
-        }
+    res = arr.map(item => {
+        return item.replace(reg1, '').replace(reg2, '.').replace(reg3, '-')
     })
-    result = result.join('')
-    return result
+
+    for (let key of res) {
+        if (key === '**********') {
+            words.push(' ')
+        } else {
+            words.push(MORSE_TABLE[key])
+        }
+    }
+    return words.join('')
 }
 
 module.exports = {
